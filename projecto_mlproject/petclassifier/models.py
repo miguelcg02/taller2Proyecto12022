028ead91a6b13f5bf8eb50003b5139f92eb16cb0
Miguel Angel Calvache Giraldo <calvache116@gmail.com>
2022-09-20 12:58:24 -0500

    todo

---
+++ projecto_mlproject/petclassifier/models.py
@@ -0,0 +1,11 @@
+from platform import architecture
+from django.db import models
+
+# Create your models here.
+
+class mlModels(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    architecture = models.FileField(upload_to='mlmodels/')
+    weights = models.FileField(upload_to='mlmodels/')
+    priority = models.IntegerField()
